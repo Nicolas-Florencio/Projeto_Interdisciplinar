3ebe83bd5bf299fcb5d16667de4973c002f06c9a
Nicolas-Florencio <nicholasflorencio17@gmail.com>
2024-11-15 19:49:23 -0300

    Conexao funcional

--- back/conexao.php
+++ back/conexao.php
@@ -1,5 +1,4 @@
 <?php
-    if(session_status())
 
     $tipo = "mysql"; //identificador do sgbd
 
@@ -7,17 +6,22 @@
     $porta = 3306; //porta do servidor
     $banco = "alergenicos"; //banco a ser usado
     
-    /*
-    $user = "php"; //nome do user
-    $pass = 'senha123'; //senha user
-    */
+    $user = "root"; //nome do user
+    $pass = '1234'; //senha user
 
     //a dsn é uma string que informa a biblioteca os dados da conexao
     $dsn = "$tipo:host=$url;dbname=$banco;port=$porta";
 
     try {
         $pdo = new PDO($dsn, $user, $pass);
+        
+        if(!$pdo) {
+            $user = "php"; //nome do user
+            $pass = 'senha123'; //senha user
 
+            //a dsn é uma string que informa a biblioteca os dados da conexao
+            $dsn = "$tipo:host=$url;dbname=$banco;port=$porta";
+        }
     } catch(PDOException $e) {
         throw new PDOException($e->getMessage(), $e->getCode());
     }
